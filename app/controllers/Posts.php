8c6b37277d7ecc30b034ca126ff13c2b166d0bb3
mjasprec14 <michaeljosephasprec1@gmail.com>
2020-08-10 21:48:51 +0800

    Added news and annoucements post functionality have it display on main/posts page

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -23,6 +23,50 @@ class Posts extends Controller {
 
               $this->view('posts/index', $data);
        }
+
+       public function add(){
+
+              if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                     $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+                     $data = [
+                            'title' => trim($_POST['title']),
+                            'body' => trim($_POST['body']),
+                            'user_id' => $_SESSION['user_id'],
+                            'title_err' => '',
+                            'body_err' => ''
+                     ];
+
+                     if(empty($data['title'])){
+                            $data['title_err'] = 'Please enter a title';
+                     }
+
+                     if(empty($data['body'])){
+                            $data['body_err'] = 'Please enter a body';
+                     }
+
+
+                     if(empty($data['body_err']) && empty($data['title_err'])){
+                            
+                            if($this->postModel->addPost($data)){
+                                   flash('post_message', 'Post Added');
+                                   redirect('posts');
+                            }else{
+                                   die('Something went wrong');
+                            }
+                     }else {
+                            $this->view('posts/add', $data);
+                     }
+
+              }else{
+                      $data = [
+                            'title' => '',
+                            'body' => ''
+                     ];
+
+                     $this->view('posts/add', $data);
+              }
+       }
 }
 
 ?>
--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -1,5 +1,7 @@
 <?php require_once APPROOT . '/views/inc/header.php'; ?>
 
+<?php flash('post_message'); ?>
+
 <div class="row mb-3">
        <div class="col-md-6">
               <h3>News and Announcements</h3>
@@ -20,11 +22,12 @@
                      <p class="card-text p-2 mb-4"><?php echo $posts->body ?></p>
 
                      <div class="row">
-                            <div class="col-md-2">
-                                   <a href="<?php echo URLROOT; ?>/posts/show<?php echo $posts->postsId; ?>" class="btn btn-dark">Read More</a>
-                            </div>
                             <div class="col-md-10">
-                                   <p class="p-2 mb-3 text-right">Written by <?php echo $posts->name; ?> on <?php echo $posts->postCreated; ?></p>
+                                   <p class="p-2 mb-3">Written by <?php echo $posts->name; ?> on <?php echo $posts->postCreated; ?></p>
+                                   
+                            </div>
+                            <div class="col-md-2">
+                                   <a href="<?php echo URLROOT; ?>/posts/show<?php echo $posts->postsId; ?>" class="btn btn-dark pull-right">Read More</a>
                             </div>
                      </div>
               </div>
