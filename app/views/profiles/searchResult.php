6cfeca9485a02e7142a97612db582991d239b8df
mjasprec14 <michaeljosephasprec1@gmail.com>
2020-08-13 15:24:48 +0800

    Added last part of displaying image for searchResult by lastname

--- app/views/profiles/searchResult.php
+++ app/views/profiles/searchResult.php
@@ -32,20 +32,28 @@
 
      <?php foreach($data['profiles'] as $profiles) : ?>
               
-              <div class="card card-body mb-3">
-                     <h6 class="card-title"><?php echo $profiles->control_no; ?></h6>
-                     <p><?php echo ucwords($profiles->last_name) . ', ' . ucwords($profiles->first_name) . ' ' . ucwords($profiles->middle_name); ?></p>
-
-                     <div class="row">
-                            <div class="col-md-10">
-                                   <p>Created by <?php echo $profiles->name; ?> on <?php echo $profiles->created_at; ?></p>
-                            </div>
-
-                            <div class="col-md-2">
-                                   <a href="<?php echo URLROOT; ?>/profiles/showProfile/<?php echo $profiles->profileId; ?>" class="btn btn-primary pull-right">View More</a>
-                            </div>
+       <div class="card card-body mb-3" style="display: <?php echo $profiles->display; ?>">
+              <div class="row">
+                     <div class="col-md-2">
+                                   <img src="<?php echo URLROOT; ?>/img/<?php echo $profiles->image; ?>" alt="" width="110" height="120">
                      </div>
-              </div>
+
+                     <div class="col-md-10">
+                                   <h6 class="card-title"><?php echo $profiles->control_no; ?></h6>
+                                   <p><?php echo ucwords($profiles->last_name) . ', ' . ucwords($profiles->first_name) . ' ' . ucwords($profiles->middle_name); ?></p>
+
+                                   <div class="row">
+                                          <div class="col-md-9">
+                                                 <p>Created by <?php echo $profiles->name; ?> on <?php echo $profiles->created_at; ?></p>
+                                          </div>
+
+                                          <div class="col-md-3">
+                                                 <a href="<?php echo URLROOT; ?>/profiles/showProfile/<?php echo $profiles->profileId; ?>" class="btn btn-primary pull-right">View More</a>
+                                          </div>
+                                   </div>
+                     </div>
+              </div>                 
+       </div>
 
        <?php endforeach; ?>
 
